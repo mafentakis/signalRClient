22661b737d81c7ba2bc84a7a3cbca4cc3b3e196f
Manolis Afentakis <mafentakis@users.noreply.github.com>
2025-09-10 12:35:44 +0200

    Update Program.cs

--- .devcontainer/signalr-server/Program.cs
+++ .devcontainer/signalr-server/Program.cs
@@ -16,12 +16,14 @@ _ = Task.Run(async () =>
 {
     while (true)
     {
-        await Task.Delay(TimeSpan.FromSeconds(random.Next(2, 5)));
-        await hubContext.Clients.All.SendAsync("EventA", Guid.NewGuid().ToString());
-        await hubContext.Clients.All.SendAsync("EventB", Guid.NewGuid().ToString());
+        await Task.Delay(TimeSpan.FromSeconds(random.Next(1, 5)));
+        await hubContext.Clients.All.SendAsync("EventA","EventA"+ Guid.NewGuid().ToString());
+        await Task.Delay(TimeSpan.FromSeconds(random.Next(1, 2)));
+        await hubContext.Clients.All.SendAsync("EventB","EventB"+ Guid.NewGuid().ToString());
     }
 });
 
 app.Run("http://0.0.0.0:8080");
 
 class TestHub : Hub { }
+
